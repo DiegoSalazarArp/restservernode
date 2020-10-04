462a76fd7ac86ad0520543799b2dca57c4876cbb
Diego Salazar <d.salazararp@gmail.com>
2020-10-03 23:06:02 -0300

    RestServer -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,21 @@
 //Puerto
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+//Entorno
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//DB
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/Cafe'
+} else {
+    urlDB = 'mongodb+srv://desdev:gottisttott1@cluster0.z43s4.mongodb.net/cafe'
+}
+
+process.env.URLDB = urlDB;
