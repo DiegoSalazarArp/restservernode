e7e0ef786c9da709c9995287feb847d2dbe5489b
Diego Salazar Arp <d.salazararp@gmail.com>
2020-11-07 21:28:01 -0300

    Subida de archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -48,7 +48,29 @@ let verificarAdmin_Role = (req, res, next) => {
     }
 }
 
+////////////////////////////////////////////////////////////////
+// verifica token img
+////////////////////////////////////////////////////////////////
+let verificaTokenImg = (req, res, next) => {
+    let token = req.query.token;
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no válido'
+                }
+            })
+        }
+
+        req.usuario = decoded.usuario;
+        next();
+    });
+};
+
 module.exports = {
     verificarToken,
-    verificarAdmin_Role
+    verificarAdmin_Role,
+    verificaTokenImg
 }
